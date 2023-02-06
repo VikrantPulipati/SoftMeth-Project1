5a360a49bd8c5f91da3f71518694588fbd308faf
Gabe Ruszala <gaberuszala@gmail.com>
2023-02-05 21:41:20 -0500

    RosterManager run() reads user input, new override methods in Student.

--- RosterManager.java
+++ RosterManager.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
 public class RosterManager {
+    public void run() {
+        System.out.println("Roster Manager running...");
+        Scanner stringScanner = new Scanner(System.in);
+        String command = stringScanner.next();
+        while (command.equals("Q") != true) {
+            if (command.equals("A") == true) {
 
+            } else if (command.equals("R") == true) {
+
+            } else if (command.equals("P") == true) {
+
+            } else if (command.equals("PS") == true) {
+
+            } else if (command.equals("PC") == true) {
+
+            } else if (command.equals("L") == true) {
+
+            } else if (command.equals("C") == true) {
+
+            } else {
+                System.out.println("You fucked up");
+                break;
+            }
+            command = stringScanner.next();
+        }
+        System.out.println("Roster Manager terminated.");
+    }
 }
--- Student.java
+++ Student.java
@@ -2,4 +2,17 @@ public class Student implements Comparable<Student> {
     private Profile profile;
     private Major major;
     private int creditCompleted;
+
+    @Override
+    public int compareTo(Student student) {
+
+    }
+    @Override
+    public String toString() {
+
+    }
+    @Override
+    public int equals(Student student) {
+
+    }
 }
